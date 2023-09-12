b4b15cc78a21a9cd7bd0fcbe3bfcbb3d8527c500
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-12 17:50:38 +0400

    add func

--- TheSearch.app/Program.cs
+++ TheSearch.app/Program.cs
@@ -8,7 +8,8 @@ var criminals = new List<Criminal>
         Lastname = "Smith",
         Height = 160,
         Weight = 50,
-        Nationality = "Indian"
+        Nationality = "Indian",
+        IsArrested = false
     },
     new()
     {
@@ -16,7 +17,8 @@ var criminals = new List<Criminal>
         Lastname = "Johnson",
         Height = 168,
         Weight = 56,
-        Nationality = "Canadian"
+        Nationality = "Canadian",
+        IsArrested = true
     },
     new()
     {
@@ -24,7 +26,8 @@ var criminals = new List<Criminal>
         Lastname = "Brown",
         Height = 183,
         Weight = 60,
-        Nationality = "Australian"
+        Nationality = "Australian",
+        IsArrested = true
     },
     new()
     {
@@ -32,7 +35,8 @@ var criminals = new List<Criminal>
         Lastname = "Wilson",
         Height = 190,
         Weight = 90,
-        Nationality = "Scottish"
+        Nationality = "Scottish",
+        IsArrested = true
     },
     new()
     {
@@ -40,16 +44,107 @@ var criminals = new List<Criminal>
         Lastname = "Clark",
         Height = 160,
         Weight = 51,
-        Nationality = "South African"
+        Nationality = "South African",
+        IsArrested = false
     }
 };
 
+var searchCriminal =
+    from c
+        in criminals
+    select new { c.Height, c.Weight, c.Nationality };
+
+return;
 
 
 /*Задача:
 У нас есть список всех преступников.
-
 Вашей программой будут пользоваться детективы.
 У детектива запрашиваются данные (рост, вес, национальность)
-  и детективу выводятся все преступники которые подходят под эти параметры,
-    но уже заключенные под стражу выводиться не должны.*/
+и детективу выводятся все преступники которые подходят под эти параметры,
+но уже заключенные под стражу выводиться не должны.*/
+
+
+var exit = false;
+
+while (!exit)
+{
+    ShowMenu();
+
+    Console.Write("Enter your choice: ");
+    var choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            ShowArrestedPeople();
+            break;
+        case "2":
+            SearchCriminal();
+            break;
+        case "3":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+    }
+
+    Console.WriteLine();
+}
+
+void ShowMenu()
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1. Show arrested people");
+    Console.WriteLine("2. Search for a criminal");
+    Console.WriteLine("3. Exit");
+}
+
+int UserInput(string message)
+{
+    Console.Write(message);
+    var input = Console.ReadLine();
+    return int.Parse(input!);
+}
+
+List<Criminal> ArrestedPeople (criminals)
+{
+    var arrestedPeople = new List<Criminal>();
+    foreach (var person in criminals)
+    {
+        if ()
+        {
+            arrestedPeople.Add(person);
+        }
+    }
+}return  
+
+
+void ShowArrestedPeople()
+{
+    Console.WriteLine("List of arrested people:");
+    // var arrestedPeople = new List<Criminal>();
+    // foreach (var person in criminals!)
+    // {
+    //     if ()
+    //     {
+    //         
+    //     }
+    // }
+
+}
+
+void SearchCriminal()
+{
+    Console.WriteLine("Search for a criminal:");
+    Console.Write("Enter height: ");
+    var height = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException("Invalid height."));
+
+    Console.Write("Enter weight: ");
+    var weight = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException("Invalid weight."));
+
+    Console.Write("Enter nationality: ");
+    var nationality = Console.ReadLine() ?? throw new InvalidOperationException("Invalid nationality.");
+    
+}
