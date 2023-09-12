ccc66ece42a7d866dddf907b4e1e242c8ea432db
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-12 16:10:39 +0400

    Initial commit

---
+++ TheSearch.app/Criminal.cs
@@ -0,0 +1,16 @@
+﻿namespace TheSearch.app;
+
+public record Criminal
+{
+    public string FirstName { get; init; }
+    public string Lastname { get; init; }
+    public int Height { get; init; }
+    public int Weight { get; init; }
+    public string Nationality { get; init; }
+    
+
+    /*public bool  IsArrested()
+    {
+        
+    }*/
+}
---
+++ TheSearch.app/Program.cs
@@ -0,0 +1,53 @@
+﻿using TheSearch.app;
+
+var criminals = new List<Criminal>
+{
+    new()
+    {
+        FirstName = "John",
+        Lastname = "Smith",
+        Height = 160,
+        Weight = 50,
+        Nationality = "Indian"
+    },
+    new()
+    {
+        FirstName = "Jane",
+        Lastname = "Johnson",
+        Height = 168,
+        Weight = 56,
+        Nationality = "Canadian"
+    },
+    new()
+    {
+        FirstName = "Michael",
+        Lastname = "Brown",
+        Height = 183,
+        Weight = 60,
+        Nationality = "Australian"
+    },
+    new()
+    {
+        FirstName = "William",
+        Lastname = "Wilson",
+        Height = 190,
+        Weight = 90,
+        Nationality = "Scottish"
+    },
+    new()
+    {
+        FirstName = "Sophia",
+        Lastname = "Clark",
+        Height = 160,
+        Weight = 51,
+        Nationality = "South African"
+    }
+};
+
+/*Задача:
+У нас есть список всех преступников.
+
+Вашей программой будут пользоваться детективы.
+У детектива запрашиваются данные (рост, вес, национальность)
+  и детективу выводятся все преступники которые подходят под эти параметры,
+    но уже заключенные под стражу выводиться не должны.*/
