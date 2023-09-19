613383030e45cdfa700c26ca23752ee860e385be
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-19 21:12:54 +0400

    add CriminalFactory.cs

---
+++ TheSearch.app/Models/CriminalFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace TheSearch.app;
+
+public class CriminalFactory
+{
+    public static Criminal CreateCriminal(
+        string firstName,
+        string lastName,
+        int height,
+        int weight,
+        string nationality,
+        bool isArrested)
+    {
+        return new Criminal
+        {
+            Id = Guid.NewGuid(),
+            FirstName = firstName,
+            LastName = lastName,
+            Height = height,
+            Weight = weight,
+            Nationality = nationality,
+            IsArrested = isArrested
+        };
+    }
+}
