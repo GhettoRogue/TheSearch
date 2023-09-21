079b564ad5b19f8557bc5d975157efbdf906c0f6
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-21 19:41:12 +0400

    add VL directory & ConsoleHelper.cs

---
+++ TheSearch.app/VL/ConsoleHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace TheSearch.app.VL;
+
+public static class ConsoleHelper
+{
+    private static void PrintLine(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ReadKey();
+    }
+    
+}
