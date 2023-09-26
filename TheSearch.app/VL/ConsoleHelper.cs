b1c412db2da45f2cfe8c65e78af593f3a0f314f8
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-26 09:14:24 +0400

    add ConsoleHelper.cs

---
+++ TheSearch.app/VL/ConsoleHelper.cs
@@ -0,0 +1,48 @@
+﻿namespace TheSearch.app.VL;
+
+public static class ConsoleHelper
+{
+    private static void PrintColorLine(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    private static void PrintLine(string message)
+    {
+        Console.Write(message);
+    }
+
+    public static string UserInput(string message)
+    {
+        PrintLine(message);
+        var input = Console.ReadLine();
+        return input!;
+    }
+    
+    public static void Print(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void PrintError(string message)
+    {
+        PrintColorLine(message, ConsoleColor.Red);
+    }
+
+    public static void PrintSuccess(string message)
+    {
+        PrintColorLine(message, ConsoleColor.Green);
+    }
+
+    public static void PrintCriminal(string message)
+    {
+        PrintColorLine(message, ConsoleColor.Blue);
+    }
+
+    public static void PrintWarning(string message)
+    {
+        PrintColorLine(message, ConsoleColor.Yellow);
+    }
+}
