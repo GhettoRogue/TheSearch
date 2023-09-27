f81b250373b39b89af54672074589f638943308d
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-27 07:27:09 +0400

    update Validator.cs

---
+++ TheSearch.app/BLL/Validator.cs
@@ -0,0 +1,35 @@
+﻿namespace TheSearch.app.BLL;
+
+public class Validator
+{
+    public static bool ValidateHeight(int height) 
+    {
+        switch (height)
+        {
+            case <= 0:
+            case < 100:
+            case > 300:
+                return false;
+            default:
+                return true;
+        }
+    }
+
+    public static bool ValidateWeight(int weight)
+    {
+        switch (weight)
+        {
+            case <= 0:
+            case < 40:
+            case > 180:
+                return false;
+            default:
+                return true;
+        }
+    }
+
+    public static bool ValidateNationality(string? nationality)
+    {
+        return !string.IsNullOrEmpty(nationality) && nationality.All(char.IsLetter);
+    }
+}
