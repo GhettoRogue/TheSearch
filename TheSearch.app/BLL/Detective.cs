be3f71dc0d2c667f72ed9495d6095f3f54d23cc5
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-29 15:31:45 +0400

    add Detective.cs

---
+++ TheSearch.app/BLL/Detective.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using TheSearch.app.Models;
+
+namespace TheSearch.app.BLL;
+
+public class Detective : IDetective
+{
+    private readonly List<Criminal> _criminals;
+
+    public Detective(List<Criminal> criminals)
+    {
+        _criminals = criminals;
+    }
+
+    public IEnumerable<Criminal> FindCriminalByParameters(int height, int weight, string? nationality)
+    {
+        var find =
+            from c in _criminals
+            where c.Height == height && c.Weight == weight && c.Nationality == nationality
+            select c;
+
+        return find;
+    }
+
+    public IEnumerable<Criminal> GetArrestedCriminals(IEnumerable<Criminal> criminal)
+    {
+        return criminal.Where(c => c.IsArrested);
+        /*var arrested =
+            from person in criminal
+            where person.IsArrested
+            select person;
+
+        return arrested;*/
+    }
+}
