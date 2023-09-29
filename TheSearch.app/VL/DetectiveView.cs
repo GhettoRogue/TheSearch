66fbe24fe6f507176ea823f7de832eb712f6f9b6
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-29 09:50:24 +0400

    upd

---
+++ TheSearch.app/VL/DetectiveView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using TheSearch.app.Models;
+
+namespace TheSearch.app.VL;
+
+public static class DetectiveView
+{
+    private static void ShowMenu()
+    {
+        Console.WriteLine("--------< Menu >--------");
+        Console.WriteLine("1. Show arrested people.");
+        Console.WriteLine("2. Search for a criminal.");
+        Console.WriteLine("0. Exit");
+        Console.WriteLine("------------------------");
+    }
+}
+    
+   
+    
+   
