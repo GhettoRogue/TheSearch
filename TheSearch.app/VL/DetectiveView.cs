ad967ca0790c50f0255d48d3721a54ff466f34a0
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-29 15:31:51 +0400

    add DetectiveView.cs

--- TheSearch.app/VL/DetectiveView.cs
+++ TheSearch.app/VL/DetectiveView.cs
@@ -1,17 +1,48 @@
-﻿namespace TheSearch.app.VL;
+﻿using TheSearch.app.BLL;
+using TheSearch.app.Models;
 
-public static class DetectiveView
+namespace TheSearch.app.VL;
+
+public abstract class DetectiveView
 {
-    private static void ShowMenu()
+    private IDetective _detective;
+
+    public DetectiveView(IDetective detective)
     {
-        Console.WriteLine("--------< Menu >--------");
-        Console.WriteLine("1. Show arrested people.");
-        Console.WriteLine("2. Search for a criminal.");
-        Console.WriteLine("0. Exit");
-        Console.WriteLine("------------------------");
+        _detective = detective;
     }
+
+    public void ShowArrestedPeople(IEnumerable<Criminal> criminals)
+    {
+        ConsoleHelper.Print("List of arrested people:");
+        var arrestedPeople = _detective?.GetArrestedCriminals(criminals);
+        foreach (var criminal in arrestedPeople!)
+        {
+            ConsoleHelper.PrintSuccess($"ID criminal: {criminal.Id}" +
+                                       $"First Name: {criminal.FirstName}" +
+                                       $",Last Name: {criminal.LastName}," +
+                                       $" Height: {criminal.Height}," +
+                                       $" Weight: {criminal.Weight}," +
+                                       $" Nationality: {criminal.Nationality}");
+        }
+    }
+
+    public void FindCriminal(int height, int weight, string? nationality)
+    {
+        var findCriminal = _detective?.FindCriminalByParameters(height, weight, nationality).ToList();
+        if (findCriminal!.Count == 0)
+        {
+            ConsoleHelper.PrintWarning("No criminal found by using these data. What would you like to do next?");
+        }
+        else if (findCriminal.Count != 0)
+        {
+            ConsoleHelper.PrintSuccess("The criminal was found using this data: ");
+            foreach (var c in findCriminal)
+            {
+                ConsoleHelper.PrintCriminal($"Height: {c.Height}," + $" Weight: {c.Weight}," +
+                                            $" Nationality: {c.Nationality}");
+            }
+        }
+    }
+    
 }
-    
-   
-    
-   
