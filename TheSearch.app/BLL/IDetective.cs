ff78e5f06d823bce73e3b7625600e406d7e1a1f5
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-29 15:32:02 +0400

    add IDetective.cs

---
+++ TheSearch.app/BLL/IDetective.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using TheSearch.app.Models;
+
+namespace TheSearch.app.BLL;
+
+public interface IDetective
+{
+    
+    IEnumerable<Criminal> FindCriminalByParameters(int height, int weight, string? nationality);
+
+    IEnumerable<Criminal> GetArrestedCriminals(IEnumerable<Criminal> criminal);
+    
+}
