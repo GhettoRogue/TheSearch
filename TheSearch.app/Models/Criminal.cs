578b41bfe0ae07b8a3f209c244054951e3d67f7a
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-10-02 10:57:27 +0400

    upd auto properties

--- TheSearch.app/Models/Criminal.cs
+++ TheSearch.app/Models/Criminal.cs
@@ -2,13 +2,13 @@
 
 public record Criminal
 {
-    public Guid Id;
-    public string? FirstName;
-    public string? LastName;
-    public int Height;
-    public int Weight;
-    public string? Nationality;
-    public bool IsArrested;
-    
+    public Guid Id { get; init; }
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+    public int Height { get; init; }
+    public int Weight { get; init; }
+    public string? Nationality { get; init; }
+    public bool IsArrested { get; init; }
+
     // public string FullName => $"{FirstName} {LastName}";
 }
