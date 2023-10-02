02b03942f7d60ee77d0b492cfca17480cebdf108
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-10-02 10:58:41 +0400

    upd IDetective.cs

--- TheSearch.app/BLL/IDetective.cs
+++ TheSearch.app/BLL/IDetective.cs
@@ -5,9 +5,6 @@ namespace TheSearch.app.BLL;
 
 public interface IDetective
 {
-    
     IEnumerable<Criminal> FindCriminalByParameters(int height, int weight, string? nationality);
-
     IEnumerable<Criminal> GetArrestedCriminals(IEnumerable<Criminal> criminal);
-    
 }
