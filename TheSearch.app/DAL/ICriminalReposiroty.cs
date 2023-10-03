8a4ab73d0851fe7d1b7afe4ce0783353c8d3a7d2
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-10-03 19:00:16 +0400

    add GetOnlyArrested() & GetNotArrested()

--- TheSearch.app/DAL/ICriminalReposiroty.cs
+++ TheSearch.app/DAL/ICriminalReposiroty.cs
@@ -5,4 +5,8 @@ namespace TheSearch.app.DAL;
 public interface ICriminalRepository
 {
     IEnumerable<Criminal> GetAll();
+
+    IEnumerable<Criminal> GetOnlyArrested();
+
+    IEnumerable<Criminal> GetNotArrested();
 }
