ad39c8111e2091ad6f28558bca09268ac1f74ac7
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-10-03 18:26:47 +0400

    reformat code

--- TheSearch.app/BLL/IDetective.cs
+++ TheSearch.app/BLL/IDetective.cs
@@ -7,4 +7,5 @@ public interface IDetective
 {
     IEnumerable<Criminal> FindCriminalByParameters(int height, int weight, string? nationality);
     IEnumerable<Criminal> GetArrestedCriminals(IEnumerable<Criminal> criminal);
+    
 }
