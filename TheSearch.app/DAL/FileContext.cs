b968c29fe91c92eec530ab0d18130fce07b62b5d
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-10-06 14:49:57 +0400

    add FileContext.cs

---
+++ TheSearch.app/DAL/FileContext.cs
@@ -0,0 +1,8 @@
+﻿namespace TheSearch.app.DAL;
+
+public static class FileContext
+{
+    public const string Criminals = "criminals.json";
+    public const string Arrested = "arrested.json";
+    public const string NotArrested = "notArrested.json";
+}
