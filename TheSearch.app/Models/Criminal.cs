71366d669667768d0b1ea8a0e47d3380d7186fc7
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-12-07 20:56:11 +0400

    remove Guid

--- TheSearch.app/Models/Criminal.cs
+++ TheSearch.app/Models/Criminal.cs
@@ -2,7 +2,7 @@
 
 public record Criminal
 {
-    public Guid Id { get; init; }
+    public uint Id { get; init; }
     public string? FirstName { get; init; }
     public string? LastName { get; init; }
     public int Height { get; init; }
--- TheSearch.app/Models/CriminalFactory.cs
+++ TheSearch.app/Models/CriminalFactory.cs
@@ -2,12 +2,12 @@
 
 public static class CriminalFactory
 {
-    public static Criminal CreateCriminal(string firstName, string lastName, int height, int weight,
+    public static Criminal CreateCriminal(uint id,string firstName, string lastName, int height, int weight,
         string nationality, bool isArrested)
     {
         return new Criminal
         {
-            Id = Guid.NewGuid(),
+            Id = id,
             FirstName = firstName,
             LastName = lastName,
             Height = height,
