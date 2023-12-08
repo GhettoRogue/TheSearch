2d1e811a5039573642d19257d8f29dd192acfda9
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-12-08 20:17:58 +0400

    add JsonDA

---
+++ TheSearch.app/DAL/Json/JsonDataAccess.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using TheSearch.app.DAL.Repository;
+using TheSearch.app.Models;
+
+namespace TheSearch.app.DAL.Json;
+
+public class JsonDataAccess
+{
+    private readonly ICriminalRepository _criminalRepository;
+
+    public JsonDataAccess(ICriminalRepository criminalRepository)
+    {
+        _criminalRepository = criminalRepository;
+    }
+
+    public void SerializeAllCriminals()
+    {
+        var json = JsonSerializer.Serialize(_criminalRepository.GetAll());
+        File.WriteAllText(JsonContext.Criminals, json);
+    }
+
+    public void SerializeArrestedCriminals()
+    {
+        var json = JsonSerializer.Serialize(_criminalRepository.GetOnlyArrested());
+        File.WriteAllText(JsonContext.Arrested, json);
+    }
+
+    public void SerializeNotArrestedCriminals()
+    {
+        var json = JsonSerializer.Serialize(_criminalRepository.GetNotArrested());
+        File.WriteAllText(JsonContext.NotArrested, json);
+    }
+
+    public void DeserializeAllCriminals()
+    {
+        var json = File.ReadAllText(JsonContext.Criminals);
+        JsonSerializer.Deserialize<List<Criminal>>(json);
+    }
+
+    public void DeserializeOnlyArrested()
+    {
+        var json = File.ReadAllText(JsonContext.Arrested);
+        JsonSerializer.Deserialize<List<Criminal>>(json);
+    }
+
+    public void DeserializeNotArrested()
+    {
+        var json = File.ReadAllText(JsonContext.NotArrested);
+        JsonSerializer.Deserialize<List<Criminal>>(json);
+    }
+}
