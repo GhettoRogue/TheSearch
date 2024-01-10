bd2991bbf3ee72a4096bef9e67204bb205ef444e
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-10 20:00:41 +0400

    add UserRepo

---
+++ TheSearch.app/DAL/Repository/User/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using TheSearch.app.IL.Interfaces.User;
+using TheSearch.app.Models.User;
+
+namespace TheSearch.app.DAL.Repository.User;
+
+public class UserRepository : IUserRepository
+{
+    private readonly List<Models.User.User> _users = new();
+
+    public IEnumerable<Models.User.User> GetAll() => _users;
+    
+    private void Add(Models.User.User user) => _users.Add(user);
+    
+    public void Initialize()
+    {
+        Add(UserFactory.CreateUser("sherlock", "holmes"));
+        Add(UserFactory.CreateUser("doctor", "watson"));
+    }
+
+}
