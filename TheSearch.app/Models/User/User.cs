f14fd088c53cda882c960849664d21da1c54d5ec
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-10 19:59:20 +0400

    add User model

---
+++ TheSearch.app/Models/User/User.cs
@@ -0,0 +1,8 @@
+﻿namespace TheSearch.app.Models.User;
+
+public record User
+{
+    public required string? Login { get; init; }
+    
+    public required string? Password { get; init; }
+}
---
+++ TheSearch.app/Models/User/UserFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace TheSearch.app.Models.User;
+
+public static class UserFactory
+{
+    public static User CreateUser(string login, string password)
+    {
+        return new User
+        {
+            Login = login,
+            Password = password
+        };
+    }
+}
