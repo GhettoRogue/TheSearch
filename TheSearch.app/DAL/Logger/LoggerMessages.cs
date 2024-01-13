0c679841deb0a58656905f8d0b20d9a12d7e1166
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-13 17:44:44 +0400

    update to static

--- TheSearch.app/DAL/Logger/LoggerMessages.cs
+++ TheSearch.app/DAL/Logger/LoggerMessages.cs
@@ -1,6 +1,6 @@
 ﻿namespace TheSearch.app.DAL.Logger;
 
-public class LoggerMessages
+public static class LoggerMessages
 {
     public const string SuccessMessage = " is now authenticated.";
     public const string FailureMessage = " is now not authenticated.";
