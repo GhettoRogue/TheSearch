288943cefcf28ec1618b853991d325dca5ba9f20
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-13 17:43:47 +0400

    add static classes UI classes

---
+++ TheSearch.app/BLL/Detective/DetectiveMessages.cs
@@ -0,0 +1,29 @@
+﻿namespace TheSearch.app.BLL.Detective;
+
+public static class DetectiveMessages
+{
+    public const int ExitFromMenu = 0;
+
+    public const string EnterChoiceDetective = "Enter your choice detective: ";
+    public const string EnterChoiceUser = "Enter your choice user: ";
+    public const string ErrorChoice = "Invalid choice. Please try again.";
+    public const string ExitMessageDetective = "Good hunting, detective.";
+    public const string ExitMessageUser = "Goodbuye, detective.";
+    public const string Arrested = "List of arrested people:";
+
+    public const string CriminalsNotFound =
+        "No criminal found by using these data. What would you like to do next?";
+
+    public const string CriminalsWasFound = "The criminal was found using this data: ";
+    public const string Search = "Search for a criminal:";
+    public const string ButtonToReturn = "(or press 0 to return to the menu)";
+    public const string EnterHeight = "Enter height: ";
+    public const string ErrorHeight = "Error: invalid height. Please try again.";
+    public const string EnterWeight = "Enter weight: ";
+    public const string ErrorWeight = "Error: invalid weight. Please try again.";
+    public const string EnterNationality = "Enter nationality: ";
+    public const string ErrorNationality = "Error: invalid nationality. Please try again.";
+    public const string UserLogin = "Enter login: ";
+    public const string UserPass = "Enter password: ";
+    public const string InvalidUserData = "Invalid credentials!";
+}
---
+++ TheSearch.app/DAL/Logger/LoggerMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace TheSearch.app.DAL.Logger;
+
+public class LoggerMessages
+{
+    public const string SuccessMessage = " is now authenticated.";
+    public const string FailureMessage = " is now not authenticated.";
+}
