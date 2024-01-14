6a3889c79e596146f8b9b0623307ac25d2c7262d
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-14 11:31:28 +0400

    add xUnit test

---
+++ UnitTestTheSearch.app/BLL/DetectiveRepositoryTest.cs
@@ -0,0 +1,50 @@
+﻿using FakeItEasy;
+using TheSearch.app.BLL.Detective;
+using TheSearch.app.IL.Interfaces.Criminal;
+using TheSearch.app.Models;
+
+namespace UnitTestTheSearch.app.BLL;
+
+public class DetectiveRepositoryTest
+{
+    [Fact]
+    public void FindCriminalByParameters_Return_Test_Positive()
+    {
+        var fakeRepository = A.Fake<ICriminalRepository>();
+        var detective = new DetectiveRepository(fakeRepository);
+
+        A.CallTo(() => fakeRepository
+                .GetAll())
+            .Returns(new List<Criminal>()
+            {
+                new()
+                {
+                    Height = 220,
+                    Weight = 190,
+                    Nationality = "Russian",
+                    FirstName = null,
+                    IsArrested = false,
+                    Id = default
+                }
+            });
+
+        var result = detective.FindCriminalByParameters(220, 190, "Russian");
+
+        Assert.Single(result);
+    }
+    
+    [Fact]
+    public void FindCriminalByParameters_Return_Test_Negative()
+    {
+        var fakeRepository = A.Fake<ICriminalRepository>();
+        var detective = new DetectiveRepository(fakeRepository);
+
+        A.CallTo(() => fakeRepository
+                .GetAll())
+                .Returns(new List<Criminal>());
+
+        var result = detective.FindCriminalByParameters(-220, -190, "");
+
+        Assert.Empty(result);
+    }
+}
