90f687610d48480fa5f43ae87a82af2cfc4e64b5
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-16 22:10:47 +0400

    update class

--- TheSearch.app/VL/DetectiveView.cs
+++ TheSearch.app/VL/DetectiveView.cs
@@ -1,86 +1,20 @@
 ﻿using TheSearch.app.BLL;
-using TheSearch.app.BLL.Authenticate.UserAuth;
+using TheSearch.app.BLL.Criminal;
 using TheSearch.app.BLL.Detective;
-using TheSearch.app.DAL.Logger;
-using TheSearch.app.DAL.Repository.Criminal;
-using TheSearch.app.DAL.Repository.User;
 using TheSearch.app.IL.Interfaces.Criminal;
-using TheSearch.app.IL.Interfaces.Detective;
-using TheSearch.app.Models;
 
 namespace TheSearch.app.VL;
 
 public class DetectiveView
 {
-    private readonly IDetective _detective;
-    private readonly ICriminalRepository _repository;
+    private readonly IUseSerializer _serializer;
 
-    private DetectiveView(IDetective detective, ICriminalRepository repository)
+    internal DetectiveView(IUseSerializer serializer)
     {
-        _detective = detective;
-        _repository = repository;
-    }
-    
-    public static void InitProject()
-    {
-        var exit = false;
-        do
-        {
-            TheSearchView.ShowMenuLogin();
-
-            switch (ConsoleHelper.UserInput(DetectiveMessages.EnterChoiceUser))
-            {
-                case "1":
-                    ConsoleHelper.ClearConsole();
-
-                    var userRepository = new UserRepository();
-                    userRepository.Initialize();
-
-                    var registeredUser = userRepository.GetAll().ToList();
-
-                    var inputLogin = ConsoleHelper.UserInput(DetectiveMessages.UserLogin);
-                    var inputPass = ConsoleHelper.UserInput(DetectiveMessages.UserPass);
-                    var authUser = registeredUser.FirstOrDefault(u => u.Login == inputLogin && u.Password == inputPass);
-                    if (authUser != null)
-                    {
-                        var auth = new DetectiveLog(new DetectiveAuth(authUser), new LogToFile());
-                        if (auth.IsAuth(authUser))
-                        {
-                            var repository = new CriminalRepository();
-                            var detective = new Detective(repository);
-                            repository.Initialize();
-
-                            var detectiveView = new DetectiveView(detective, repository);
-                            detectiveView.ShowDetectiveMenu();
-                        }
-                        else
-                        {
-                            auth.IsAuth(authUser);
-                            Console.WriteLine("Invalid login or password");
-                            Environment.Exit(1);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid login or password");
-                        Environment.Exit(1);
-                    }
-
-                    break;
-                case "0":
-                    exit = true;
-                    break;
-                default:
-                    ConsoleHelper.PrintError(DetectiveMessages.InvalidUserData);
-                    break;
-            }
-
-            ConsoleHelper.ClearConsole();
-            ConsoleHelper.PrintSuccess(DetectiveMessages.ExitMessageUser);
-        } while (!exit);
+        _serializer = serializer;
     }
 
-    private void ShowDetectiveMenu()
+    internal void ShowDetectiveMenu()
     {
         ConsoleHelper.ClearConsole();
         var exit = false;
@@ -90,7 +24,7 @@ public class DetectiveView
             switch (ConsoleHelper.UserInput(DetectiveMessages.EnterChoiceDetective))
             {
                 case "1":
-                    ShowArrestedPeople(_repository.GetAll());
+                    ShowArrestedPeople();
                     break;
                 case "2":
                     SearchCriminal();
@@ -99,6 +33,7 @@ public class DetectiveView
                     exit = true;
                     break;
                 default:
+                    ConsoleHelper.ClearConsole();
                     ConsoleHelper.PrintError(DetectiveMessages.ErrorChoice);
                     break;
             }
@@ -108,26 +43,35 @@ public class DetectiveView
         ConsoleHelper.PrintSuccess(DetectiveMessages.ExitMessageDetective);
     }
 
-    private void ShowArrestedPeople(IEnumerable<Criminal> criminals)
+    private void ShowArrestedPeople()
     {
         ConsoleHelper.ClearConsole();
         ConsoleHelper.Print(DetectiveMessages.Arrested);
-        var arrestedPeople = _detective.GetArrestedCriminals(criminals);
-        foreach (var criminal in arrestedPeople)
+        try
         {
-            ConsoleHelper.PrintSuccess(
-                $"ID criminal: [{criminal.Id}]," +
-                $" First Name: {criminal.FirstName}," +
-                $" Last Name: {criminal.LastName}," +
-                $" Height: {criminal.Height}," +
-                $" Weight: {criminal.Weight}," +
-                $" Nationality: {criminal.Nationality}");
+            var arrestedPeople =
+                (_serializer.DeserializeOnlyArrested() ?? throw new InvalidOperationException()).ToList();
+            arrestedPeople.ForEach(criminal => ConsoleHelper.PrintSuccess(
+                $"{CriminalMessages.Id} [{criminal.Id}]," +
+                $" {CriminalMessages.FirstName} {criminal.FirstName}," +
+                $" {CriminalMessages.LastName} {criminal.LastName}," +
+                $" {CriminalMessages.Height} {criminal.Height}," +
+                $" {CriminalMessages.Weight} {criminal.Weight}," +
+                $" {CriminalMessages.Nationality} {criminal.Nationality}"));
+        }
+        catch (ArgumentNullException ex)
+        {
+            ConsoleHelper.PrintError(ex.Message);
+            throw;
         }
     }
 
     private void FindCriminal(int height, int weight, string? nationality)
     {
-        var findCriminal = _detective.FindCriminalByParameters(height, weight, nationality).ToList();
+        var findCriminal = (_serializer
+                .DeserializeAllCriminals() ?? throw new InvalidOperationException())
+            .Where(c => c.Height == height && c.Weight == weight && c.Nationality == nationality)
+            .ToList();
         if (findCriminal.Count == 0)
         {
             ConsoleHelper.ClearConsole();
@@ -138,17 +82,12 @@ public class DetectiveView
             ConsoleHelper.ClearConsole();
             ConsoleHelper.PrintSuccess(DetectiveMessages.CriminalsWasFound);
             findCriminal.ForEach(c => ConsoleHelper.PrintCriminal
-            ($"[Height: {c.Height}," + $" Weight: {c.Weight}," + $" Nationality: {c.Nationality}]" +
-             $" - [{c.FirstName} {c.LastName}] - |IsArrested:{c.IsArrested}| "));
+            ($"[{CriminalMessages.Height} {c.Height}," +
+             $" {CriminalMessages.Weight} {c.Weight}," +
+             $" {CriminalMessages.Nationality} {c.Nationality}]" +
+             $" - [{c.FirstName} {c.LastName}] - |{CriminalMessages.IsArrested}{c.IsArrested}| "));
         }
     }
-    /*private List<Criminal> FindCriminal(int height, int weight, string? nationality)
-    {
-        var findCriminal = _detective.FindCriminalByParameters(height, weight, nationality).ToList();
-        if (!findCriminal.Any()) throw new ArgumentNullException(); 
-        
-        return findCriminal;
-    }*/
 
     private void SearchCriminal()
     {
@@ -157,7 +96,6 @@ public class DetectiveView
         ConsoleHelper.PrintWarning(DetectiveMessages.ButtonToReturn);
 
         int height;
-
         while (true)
         {
             ConsoleHelper.PrintLine(DetectiveMessages.EnterHeight);
