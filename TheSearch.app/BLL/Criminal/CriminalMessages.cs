01b4256033c0f597957e9f92e451e062073ec53a
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-19 20:46:24 +0400

    add Criminal handler

---
+++ TheSearch.app/BLL/Criminal/CriminalMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace TheSearch.app.BLL.Criminal;
+
+public static class CriminalMessages
+{
+    public const string Id = "ID criminal: ";
+    public const string FirstName = "First Name: ";
+    public const string LastName = "Last Name: ";
+    public const string Height = "Height: ";
+    public const string Weight = "Weight: ";
+    public const string Nationality = "Nationality: ";
+    public const string IsArrested = "IsArrested: ";
+}
