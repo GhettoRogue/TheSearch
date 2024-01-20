5c76e21ca8076ccdd1bbda89819912b673bc143d
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-20 14:37:03 +0400

    remove invalid condition

--- TheSearch.app/BLL/Validator.cs
+++ TheSearch.app/BLL/Validator.cs
@@ -4,9 +4,9 @@ public static class Validator
 {
     #region ValidateCriminals
 
-    public static bool ValidateHeight(int height) => height is > 0 and >= 100 and <= 290;
+    public static bool ValidateHeight(int height) => height is >= 100 and <= 290;
 
-    public static bool ValidateWeight(int weight) => weight is > 0 and >= 40 and <= 180;
+    public static bool ValidateWeight(int weight) => weight is >= 40 and <= 180;
 
     public static bool ValidateNationality(string? nationality) =>
         !string.IsNullOrEmpty(nationality) && nationality.All(char.IsLetter);
