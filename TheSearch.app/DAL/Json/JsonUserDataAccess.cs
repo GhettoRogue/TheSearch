01ede941b48de4fd4c077af289179235a337ebfd
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-20 14:57:25 +0400

    replace throw return new Exception

---
+++ TheSearch.app/DAL/Json/JsonUserDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using TheSearch.app.IL.Interfaces.User;
+using TheSearch.app.Models.User;
+using TheSearch.app.VL;
+
+namespace TheSearch.app.DAL.Json;
+
+public class JsonUserDataAccess : IUserSerializer
+{
+    private readonly IUserRepository _userRepository;
+
+    public JsonUserDataAccess(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    #region User
+
+    public void SerializeUser()
+    {
+        var json = JsonSerializer.Serialize(_userRepository.GetAll());
+        File.WriteAllText(JsonContext.UserAuthDataPath, json);
+    }
+
+    public IEnumerable<User>? DeserializeUser()
+    {
+        try
+        {
+            var json = File.ReadAllText(JsonContext.UserAuthDataPath);
+            return JsonSerializer.Deserialize<IEnumerable<User>>(json);
+        }
+        catch (ArgumentNullException ex)
+        {
+            ConsoleHelper.PrintError(ex.Message);
+        }
+
+        throw new InvalidOperationException();
+    }
+
+    #endregion
+}
