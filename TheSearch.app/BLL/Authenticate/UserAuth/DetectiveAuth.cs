9fdff352c12f2c2770eb14d3d8120740476cbcc5
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-20 15:04:47 +0400

    add future realisation

---
+++ TheSearch.app/BLL/Authenticate/UserAuth/DetectiveAuth.cs
@@ -0,0 +1,10 @@
+﻿using TheSearch.app.IL.Interfaces.Auth;
+using TheSearch.app.Models.User;
+
+namespace TheSearch.app.BLL.Authenticate.UserAuth;
+
+public class DetectiveAuth : IAuth
+{
+    public bool IsUserIn(User? user) => user != null;
+    //unused class & interface
+}
