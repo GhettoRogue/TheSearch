8eb3e00e8febc4d3b27a48ea874ef110dae33030
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-20 15:03:49 +0400

    add main check logic & log to .txt

---
+++ TheSearch.app/DAL/Logger/DetectiveLog.cs
@@ -0,0 +1,26 @@
+﻿using TheSearch.app.IL.Interfaces.Logger;
+using TheSearch.app.Models.User;
+
+namespace TheSearch.app.DAL.Logger;
+
+public class DetectiveLog
+{
+    private readonly ILoggerMessage _loggerMessage;
+
+    public DetectiveLog(ILoggerMessage loggerMessage)
+    {
+        _loggerMessage = loggerMessage;
+    }
+
+    public bool IsAuth(User? user)
+    {
+        if (user == null)
+        {
+            _loggerMessage.LoggerError(user?.Login + LoggerMessages.FailureMessage);
+            return false;
+        }
+
+        _loggerMessage.LoggerSuccess(user.Login + LoggerMessages.SuccessMessage);
+        return true;
+    }
+}
---
+++ TheSearch.app/DAL/Logger/LogToFile.cs
@@ -0,0 +1,24 @@
+﻿using TheSearch.app.IL.Interfaces.Logger;
+
+namespace TheSearch.app.DAL.Logger;
+
+public class LogToFile : ILoggerMessage
+{
+    private const string DetectiveLogPath = "authLog.txt";
+
+    private static void Write(string message)
+    {
+        using var file = new StreamWriter(DetectiveLogPath, true);
+        file.WriteLine($"{DateTime.Now:g} {message}");
+    }
+
+    public void LoggerError(string message)
+    {
+        Write($"[{nameof(LoggerError).ToUpper()}] {message}");
+    }
+
+    public void LoggerSuccess(string message)
+    {
+        Write($"[{nameof(LoggerSuccess).ToUpper()}] {message}");
+    }
+}
