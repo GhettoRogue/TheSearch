f1655206f700410f5e76ce4d69cdd1a4f2a0f821
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-21 16:57:06 +0400

    refactoring UserFactoryTest.cs

--- UnitTestTheSearch.app/Models/UserFactoryTest.cs
+++ UnitTestTheSearch.app/Models/UserFactoryTest.cs
@@ -1,20 +1,30 @@
 ﻿using AutoFixture;
+using AutoFixture.Kernel;
+using TheSearch.app.Models.User;
 
 namespace UnitTestTheSearch.app.Models;
 
 public class UserFactoryTest
 {
+    private static User CreateTestUser(ISpecimenBuilder fixture)
+    {
+        return new User
+        {
+            Login = fixture.Create<string>(),
+            Password = fixture.Create<string>()
+        };
+    }
+
     [Fact]
     public void Should_CreateUser_Good()
     {
         var fixture = new Fixture();
 
-        var login = fixture.Create<string>();
-        var password = fixture.Create<string>();
+        var expUser = CreateTestUser(fixture);
 
-        var result = TheSearch.app.Models.User.UserFactory.CreateUser(login, password);
+        var actualUser = UserFactory.CreateUser(expUser.Login, expUser.Password);
 
-        Assert.Equal(login, result.Login);
-        Assert.Equal(password, result.Password);
+
+        Assert.StrictEqual(expUser, actualUser);
     }
 }
