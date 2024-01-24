c8d20cac28a121309519f3121391d511733d4dc4
Vladimir Ignatiev <vladim.ignatiew2010@yandex.ru>
2024-01-24 14:58:15 +0400

    update

--- UnitTestTheSearch.app/Models/CriminalFactoryTest.cs
+++ UnitTestTheSearch.app/Models/CriminalFactoryTest.cs
@@ -1,6 +1,7 @@
-﻿namespace UnitTestTheSearch.app.Models;
+﻿using AutoFixture;
+using TheSearch.app.Models;
 
-using AutoFixture;
+namespace UnitTestTheSearch.app.Models;
 
 public class CriminalFactoryTest
 {
@@ -8,7 +9,7 @@ public class CriminalFactoryTest
     public void Should_CreateCriminal_Good()
     {
         var fixture = new Fixture();
-        
+
         var firstName = fixture.Create<string>();
         var lastName = fixture.Create<string>();
         var height = fixture.Create<int>();
@@ -16,14 +17,8 @@ public class CriminalFactoryTest
         var nationality = fixture.Create<string>();
         var isArrested = fixture.Create<bool>();
 
-        var result = TheSearch.app.Models.CriminalFactory.CreateCriminal(firstName, lastName, height, weight, nationality, isArrested);
-        
-        Assert.Equal(firstName, result.FirstName);
-        Assert.Equal(lastName, result.LastName);
-        Assert.Equal(height, result.Height);
-        Assert.Equal(weight, result.Weight);
-        Assert.Equal(nationality, result.Nationality);
-        Assert.Equal(isArrested, result.IsArrested);
+        var expCriminal = CriminalFactory.CreateCriminal(firstName, lastName, height, weight, nationality, isArrested);
 
+        Assert.Equal(firstName, expCriminal.FirstName);
     }
 }
